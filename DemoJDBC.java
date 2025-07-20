3971579d53972910d170c8b77461c6bc86a0bcfc
Ahmad Raza Ansari <102856262+Ahmadraza301@users.noreply.github.com>
2025-07-20 16:54:12 +0530

    Add files via upload

--- DemoJDBC.java
+++ DemoJDBC.java
@@ -1,43 +1,26 @@
 import java.sql.*;
 
 public class DemoJDBC {
-    public static void main(String[] args) throws Exception{
-        /*
-        import the packages
-        load and register
-        create connection
-        create statement
-        execute statement
-        process the result
-        close
-        */
+    public static void main(String[] args) throws Exception {
+        int sid = 101;
+        String sname = "yasmin khan";
+        int marks = 88;
+
         String url = "jdbc:postgresql://localhost:5432/students";
         String uname = "postgres";
         String pass = "421302";
-        String sql = "select * from student";
+        String sql = "insert into student values (?, ?, ?)";
 
-//        Class.forName("org.postgresql.Driver");
         Connection con = DriverManager.getConnection(url, uname, pass);
-        System.out.println("connection established");
-        Statement st = con.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-//        rs.next();
+        PreparedStatement st = con.prepareStatement(sql);
 
-//       String name =rs.getString("sname");
-//       System.out.println("Name of the Student " +name);
-        while(rs.next()){
-            System.out.println(rs.getInt(1) + " - ");
-            System.out.println(rs.getString(2) + " - ");
-            System.out.println(rs.getInt(3));
+        st.setInt(1, sid);      // Set 1st parameter (sid)
+        st.setString(2, sname); // Set 2nd parameter (sname)
+        st.setInt(3, marks);    // Set 3rd parameter (marks)
 
+        st.executeUpdate();
 
-        }
-
+        st.close();
         con.close();
-        System.out.println("Connection closed");
-
-
-
-
     }
 }
